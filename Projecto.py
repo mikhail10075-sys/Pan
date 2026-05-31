5c272c998a0b5593f162250a3e2eb0d2ad2daf7b
mikhail10075-sys <mikhail10075@gmail.com>
2026-05-31 21:23:37 +0530

    Add files via upload

---
+++ Projecto.py
@@ -0,0 +1,12 @@
+def shutdown():
+    print("Shuting down...")
+
+while True:
+ x = input("Have all tabs and windows been closed? " )
+
+
+ if x=="Yes":
+     shutdown()
+     break
+ else:
+     print("Kindly close windows and tabs.")
